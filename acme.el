d1dcd5ce3b86fb7d7101b2d449110b8e36c98aee
amirrezaask <raskarpour@gmail.com>
2020-08-24 18:03:30 +0430

    now executing commands works and result goes to a new buffer

--- acme.el
+++ acme.el
@@ -1,31 +1,32 @@
-(defun active-region ()
+;;;###autoload
+(defun acme/--active-region ()
   (interactive)
   (buffer-substring (region-beginning) (region-end)))
 
 ;;;###autoload
-(defun acme/--exec-command-from-region ()
+(defun acme/--exec-command-from-region (cmd)
   (interactive)
-  (shell-command-to-string (active-region)))
+  (insert (shell-command-to-string cmd)))
 
-(defun acme/--new-window-and-switch ()
+(defun acme/--new-window-and-switch (cmd)
   "Create a new window and switch to it."
-  (split-window-right)
-  (other-window 1))
+  (switch-to-buffer-other-window (format "*ACME:Exec %s" cmd)))
 
-(defun acme/exec-command ()
+(defun acme/exec-command-in-region ()
   "Execute selected command in region."
   (interactive)
-  (acme/--new-window-and-switch)
-  (acme/--exec-command-from-region))
+  (let ((cmd (acme/--active-region)))
+    (acme/--new-window-and-switch cmd)
+    (acme/--exec-command-from-region cmd)))
 
 (defun acme/initialize ()
   "Initialize acme-mode."
-  (global-set-key [mouse-2] 'exec-command)
-  (global-set-key (kbd "C-s-e") 'exec-command))
+  (global-set-key [mouse-2] 'acme/exec-command-in-region)
+  (global-set-key (kbd "C-s-e") 'acme/exec-command-in-region))
   
-(define-minor-mode acme-mode "Acme simulation layer for Emacs."
+(define-minor-mode acme-mode
+  "Acme simulation layer for Emacs."
   :global t
-  :after-hook
-  (acme/initialize))
+  :after-hook (acme/initialize))
 
 (provide 'acme)
