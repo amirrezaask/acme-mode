65194be29bbcaa24ce1b74cbd1cf0bd04888458f
amirrezaask <raskarpour@gmail.com>
2020-08-24 17:34:20 +0430

    add basic command execution with middle click

--- acme.el
+++ acme.el
@@ -2,18 +2,30 @@
   (interactive)
   (buffer-substring (region-beginning) (region-end)))
 
-(defun exec-command ()
+;;;###autoload
+(defun acme/--exec-command-from-region ()
   (interactive)
-  (message (shell-command-to-string (active-region))))
+  (shell-command-to-string (active-region)))
 
-(defun set-keys ()
-  (global-set-key [mouse-2] 'exec-command))
+(defun acme/--new-window-and-switch ()
+  "Create a new window and switch to it."
+  (split-window-right)
+  (other-window 1))
 
+(defun acme/exec-command ()
+  "Execute selected command in region."
+  (interactive)
+  (acme/--new-window-and-switch)
+  (acme/--exec-command-from-region))
 
+(defun acme/initialize ()
+  "Initialize acme-mode."
+  (global-set-key [mouse-2] 'exec-command)
+  (global-set-key (kbd "C-s-e") 'exec-command))
+  
+(define-minor-mode acme-mode "Acme simulation layer for Emacs."
+  :global t
+  :after-hook
+  (acme/initialize))
 
-
-
-echo 'salam'
-
-
-(provide 'init)
+(provide 'acme)
